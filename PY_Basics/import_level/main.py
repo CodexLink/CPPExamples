cbadd84786c086eb2a9ea8e7ea646e2accb61bba
Janrey Licas <janjan.licas@gmail.com>
2021-09-20 02:56:45 +0000

    Updated: Added .gitignore and Unit Test Case Assignment

---
+++ PY_Basics/import_level/main.py
@@ -0,0 +1,2 @@
+print(f"Hello! My current name is {__name__}! | from {__file__}")
+import second
---
+++ PY_Basics/import_level/second.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    print(f"Hello! My current name is {__name__}! | from {__file__}")
+else:
+    print("Not a main package.")
